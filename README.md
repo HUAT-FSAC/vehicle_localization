f4b83dbff5d238814759ddefea40caa24bbe4c08
NekoRectifier <hyx123250@gmail.com>
2023-07-26 14:15:34 +0800

    feat: output formatted data into spec csv files

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # gps_imu_fusion
 
+## depenencies
+
+- common_msgs (HUAT_ASENSING.h)
+
 ## Todo
 
 - update cmakelist file: add exec
+
+- 2 exec files, using launch file to initiate
+
+## ESKF Requirements:
+
+- acc(m/s^2): x,y,z (including gravity)
+- gps: lat(deg), lon(deg), alt(m), vN,vE,vD(m/s) (North, East, Down)
+- gyro(deg/s): x,y,z
+- gps_time?
+  the timestamp that its data updates
--- src/msg_handler.cpp
+++ src/msg_handler.cpp
@@ -1,19 +1,36 @@
 #include "ros/ros.h"
+#include <fstream>
+#include <string>
+#include "common_msgs/HUAT_ASENSING.h"
 
-ros::NodeHandle nh;
+void ins570dCallback(const common_msgs::HUAT_ASENSING::ConstPtr &msgs);
 
+// TODO: relocate the csv file to root or tmp folder (when running in reak vehicle)
+std::ofstream gps_out("../../../src/gps_imu_fusion/data/gps.csv", std::ios::app);
+std::ofstream gyro_out("../../../src/gps_imu_fusion/data/gyro.csv", std::ios::app);
+std::ofstream acc_out("../../../src/gps_imu_fusion/data/acc.csv", std::ios::app);
 
 void ins570dCallback(const common_msgs::HUAT_ASENSING::ConstPtr &msgs)
 {
     // since the gps and imu data are from the same source, their created time should be the same too.
     // or two data are updated differently, need to judge mannuly.
 
+    // precision more than 10 is useless
+    gps_out << std::fixed << std::setprecision(10) << 
+        msgs->latitude << "," << msgs->longitude << "," << msgs->altitude << "," << 
+        msgs -> north_velocity << "," << msgs -> east_velocity << "," << msgs -> ground_velocity << std::endl;
+    
+    acc_out << std::fixed << std::setprecision(10) << msgs->x_acc << "," << msgs->y_acc << "," << msgs->z_acc << std::endl;
+    gyro_out << std::fixed << std::setprecision(10) << msgs->x_angular_velocity << "," << msgs->y_angular_velocity << "," << msgs->z_angular_velocity << std::endl;
 }
 
-int msgHandlerInit()
+int main(int argc, char *argv[])
 {
-    // do 
+    ros::init(argc, argv, "gps_imu_fusion");
 
-    // designed for INS570D IMU&GPS Module
-    ros::Subscriber INSsub = nh.subscribe<common_msgs::HUAT_ASENSING>("/INS/ASENSING_INS", 1, doINSMsg);
+    ros::NodeHandle nh;
+    ros::Subscriber INSsub = nh.subscribe<common_msgs::HUAT_ASENSING>("/INS/ASENSING_INS", 1, ins570dCallback);
+
+    std::cout << "spining..." << std::endl;
+    ros::spin();
 }
